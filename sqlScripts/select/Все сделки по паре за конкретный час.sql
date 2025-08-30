957b9bd26b1cdab53614ac6390d1507cec6260a0
AlexPaturov <alexey.paturov@gmail.com>
2025-08-30 21:04:50 +0300

    Test deploy

--- sqlScripts/select/Все сделки по паре за конкретный час.sql
+++ sqlScripts/select/Все сделки по паре за конкретный час.sql
@@ -1,14 +1,8 @@
 SELECT
-    to_timestamp("TradeTime" / 1000.0) AS "TradeDateTime",
-    "Price",
-    "Quantity",
-    "QuoteQuantity",
-    "IsBuyerMaker"
+    count(*)
 FROM
     public."Trades"
 WHERE
     "Symbol" = :symbol AND
-    "TradeTime" BETWEEN (EXTRACT(EPOCH FROM '2025-08-20 15:00:00'::TIMESTAMPTZ) * 1000)::BIGINT 
-                  AND (EXTRACT(EPOCH FROM '2025-08-20 15:59:59'::TIMESTAMPTZ) * 1000)::BIGINT
-ORDER BY
-    "TradeTime" ASC;
+    "TradeTime" BETWEEN (EXTRACT(EPOCH FROM '2025-08-30 14:04:00'::TIMESTAMPTZ) * 1000)::BIGINT 
+                  AND (EXTRACT(EPOCH FROM '2025-08-30 14:05:00'::TIMESTAMPTZ) * 1000)::BIGINT;
