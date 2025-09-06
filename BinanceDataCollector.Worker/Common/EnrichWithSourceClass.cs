528f673d3ae7b523fd1362467777c5067125c222
AlexPaturov <alexey.paturov@gmail.com>
2025-09-06 11:53:22 +0300

    Обрезаем путь к классу только до имени класса - рабочий

---
+++ BinanceDataCollector.Worker/Common/EnrichWithSourceClass.cs
@@ -0,0 +1,26 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace BinanceDataCollector.Worker.Common;
+
+public class EnrichWithSourceClass : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        // Extract just the class from the end of the source context property
+        if (logEvent.Properties.TryGetValue("SourceContext", out LogEventPropertyValue? sourceContextValue)
+            && sourceContextValue is ScalarValue scalarValue)
+        {
+            // Get source context as literal value using "l" serilog formatter, to avoid wrapping in quote characters
+            string sourceContext = scalarValue.ToString("l", null);
+
+            int start = sourceContext.LastIndexOf('.');
+            start = (start >= 0) ? (start + 1) : 0;
+
+            string sourceClass = sourceContext[start..];
+            LogEventProperty enrichProperty = propertyFactory.CreateProperty("SourceClass", sourceClass);
+
+            logEvent.AddOrUpdateProperty(enrichProperty);
+        }
+    }
+}
