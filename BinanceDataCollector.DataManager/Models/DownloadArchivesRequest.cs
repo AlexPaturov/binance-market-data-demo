d51d1e56dcb9c4fdeeffb6b635a87ab294556033
AlexPaturov <alexey.paturov@gmail.com>
2025-09-27 21:27:36 +0300

    Для пакетной загрузки архивов

---
+++ BinanceDataCollector.DataManager/Models/DownloadArchivesRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace BinanceDataCollector.DataManager.Models;
+
+/// <summary>
+/// Для загрузки архивов по всему списку активных символов
+/// </summary>
+public class DownloadArchivesRequest
+{
+    public string? Symbol { get; set; }
+    public bool DownloadAll { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+}
