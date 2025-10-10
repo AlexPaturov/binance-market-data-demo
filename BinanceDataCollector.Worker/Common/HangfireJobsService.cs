1dc7aaeca1794e94b01a4c7a585835ac8aafee67
AlexPaturov <alexey.paturov@gmail.com>
2025-10-10 23:14:34 +0300

    Uncommented update-symbol block

--- BinanceDataCollector.Worker/Common/HangfireJobsService.cs
+++ BinanceDataCollector.Worker/Common/HangfireJobsService.cs
@@ -29,12 +29,12 @@ public class HangfireJobsService : IHostedService
         _recurringJobManager.RemoveIfExists("update-symbols-history");
 
         // Вместо статического RecurringJob используем IRecurringJobManager из DI
-        //_recurringJobManager.AddOrUpdate<SymbolUpdateWorker>(
-        //    "update-symbols",
-        //    worker => worker.ScanMarketAndUpdateSymbolsAsync(),
-        //    Cron.Daily(), // Запускать раз в день
-        //    new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
-        //);
+        _recurringJobManager.AddOrUpdate<SymbolUpdateWorker>(
+            "update-symbols",
+            worker => worker.ScanMarketAndUpdateSymbolsAsync(),
+            Cron.Daily(), // Запускать раз в день
+            new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
+        );
 
         // "historical-audit": Запускать ОДИН РАЗ В ДЕНЬ в 00:00 по киевскому времени (примерно 22:00 UTC)
         //_recurringJobManager.AddOrUpdate<HistoricalAuditorWorker>(
