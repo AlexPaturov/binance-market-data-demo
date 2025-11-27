b7c9a32e418e63d4cf06d78da2fbb3d2b6125b1c
AlexPaturov <alexey.paturov@gmail.com>
2025-11-27 20:05:57 +0200

    Сделал актвным весь функционал job's

--- src/BinanceDataCollector.Worker/Common/HangfireJobsService.cs
+++ src/BinanceDataCollector.Worker/Common/HangfireJobsService.cs
@@ -29,49 +29,49 @@ public class HangfireJobsService : IHostedService
         _recurringJobManager.RemoveIfExists("update-symbols-history");
 
         // Вместо статического RecurringJob используем IRecurringJobManager из DI
-        // _recurringJobManager.AddOrUpdate<SymbolUpdateWorker>(
-        //     "update-symbols",
-        //     worker => worker.ScanMarketAndUpdateSymbolsAsync(),
-        //     Cron.Daily(), // Запускать раз в день
-        //     new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
-        // );
+        _recurringJobManager.AddOrUpdate<SymbolUpdateWorker>(
+            "update-symbols",
+            worker => worker.ScanMarketAndUpdateSymbolsAsync(),
+            Cron.Daily(), // Запускать раз в день
+            new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
+        );
 
         // "historical-audit": Запускать ОДИН РАЗ В ДЕНЬ в 00:00 по киевскому времени (примерно 22:00 UTC)
-        //_recurringJobManager.AddOrUpdate<HistoricalAuditorWorker>(
-        //    "historical-audit",
-        //    worker => worker.AuditNextBatchAsync(),
-        //    _isDevelopment ? "*/30 * * * *" : "0 */6 * * *",  // prod каждые 6 часов по UTC 6,12,18,00 : dev каждые 30 минут
-        //    new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
-        //);
+        _recurringJobManager.AddOrUpdate<HistoricalAuditorWorker>(
+            "historical-audit",
+            worker => worker.AuditNextBatchAsync(),
+            _isDevelopment ? "*/30 * * * *" : "0 */6 * * *",  // prod каждые 6 часов по UTC 6,12,18,00 : dev каждые 30 минут
+            new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
+        );
 
         // "quick_audit": Каждые 10 минут. Проверять все символы на неполноту в рамках 24 часового окна
-        //_recurringJobManager.AddOrUpdate<QuickAuditorWorker>(
-        //    "quick_audit",
-        //    worker => worker.CheckAndFillRecentGapsAsync(),
-        //     _isDevelopment ? "*/2 * * * *" : "*/10 * * * *", // Если dev - каждые 2 минуты, если prod - каждые 10 минут
-        //    new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
-        //);
+        _recurringJobManager.AddOrUpdate<QuickAuditorWorker>(
+            "quick_audit",
+            worker => worker.CheckAndFillRecentGapsAsync(),
+             _isDevelopment ? "*/2 * * * *" : "*/10 * * * *", // Если dev - каждые 2 минуты, если prod - каждые 10 минут
+            new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
+        );
 
-        // _recurringJobManager.AddOrUpdate<AuditInitializationWorker>(
-        //     "audit-initializer",
-        //     worker => worker.CreateWatermarksForNewSymbolsAsync(),
-        //     Cron.Daily(), // Тоже раз в день
-        //     new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
-        // );
+         _recurringJobManager.AddOrUpdate<AuditInitializationWorker>(
+            "audit-initializer",
+            worker => worker.CreateWatermarksForNewSymbolsAsync(),
+            Cron.Daily(), // Тоже раз в день
+            new RecurringJobOptions { TimeZone = TimeZoneInfo.Utc }
+        );
 
         // Агрегация свечей - каждую минуту
-        // _recurringJobManager.AddOrUpdate<OhlcvAggregatorWorker>(
-        //     "ohlcv-aggregator",
-        //     worker => worker.AggregateNextBatchAsync(),
-        //     Cron.Minutely() // Каждую минуту
-        // );
+        _recurringJobManager.AddOrUpdate<OhlcvAggregatorWorker>(
+            "ohlcv-aggregator",
+            worker => worker.AggregateNextBatchAsync(),
+            Cron.Minutely() // Каждую минуту
+        );
 
         // Расчет индикаторов - каждые 2 минуты
-        // _recurringJobManager.AddOrUpdate<FeatureCalculatorWorker>(
-        //     "feature-calculator",
-        //     worker => worker.CalculateFeaturesAsync(),
-        //     "*/2 * * * *" // Каждые 2 минуты
-        // );
+        _recurringJobManager.AddOrUpdate<FeatureCalculatorWorker>(
+            "feature-calculator",
+            worker => worker.CalculateFeaturesAsync(),
+            "*/2 * * * *" // Каждые 2 минуты
+        );
 
         _logger.LogInformation("Периодические задачи зарегистрированы.");
 
