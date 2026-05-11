79457e0961efe1306fc1bb44b6201b6969fca8f0
AlexPaturov <alexey.paturov@gmail.com>
2026-05-11 19:54:14 +0300

    chore: auth commented

--- src/BinanceDataCollector.DataManager/Controllers/HomeController.cs
+++ src/BinanceDataCollector.DataManager/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BinanceDataCollector.DataManager.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
